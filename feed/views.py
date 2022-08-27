ff4650b299ba0de461b0f3e66b96833ae65d00cf
ZaeemT <zaeemtauqir@hotmail.com>
2022-08-27 15:52:25 +0500

    Made CreateNewPost view.

--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 
 from .models import Post
 
@@ -15,3 +16,7 @@ class PostDetailView(DetailView):
     model = Post
     context_object_name = "post"
     
+class CreateNewPost(CreateView):
+    model = Post
+    template_name = "feed/create.html"
+    fields = ['text'] 
